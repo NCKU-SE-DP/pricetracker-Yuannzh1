33145f96e2552ad50b4c34c29a15216313f36bbe
yuanzhixu847@gmail.com <yuanzhixu847@gmail.com>
2024-12-03 03:22:28 +0800

    Add support for in-memory SQLite database for testing

--- backend/src/database.py
+++ backend/src/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///news_database.db"
+DATABASE_URL = "sqlite:///:memory:"
 engine = create_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
