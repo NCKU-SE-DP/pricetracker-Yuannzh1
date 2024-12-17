a06911044c004633efa6d57b7bd1d8812ee0a96e
Yuannzh1 <yuanzhixu847@gmail.com>
2024-12-17 22:21:29 +0800

    set the log format

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,1 +1,4 @@
 DSN = "https://f5722f124db49960efdc4854a065bb91@o4508454840041472.ingest.de.sentry.io/4508454849675344"
+# 設定 Log 格式
+LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+LOG_FILE = "PriceTracker.log"
