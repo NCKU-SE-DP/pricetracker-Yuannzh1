ea10443f122f8debfc0792f4ff9cec4ed89d8087
Yuannzh1 <yuanzhixu847@gmail.com>
2024-12-17 22:28:14 +0800

    move sentry_sdk initializng to logging_config.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -13,14 +13,6 @@ from src.price.router import router as price_router
 from src.news.dependencies import fetch_news_info_by_search_term
 from src.crawler.udn_crawler import UDNCrawler
 from src.llm_client.openai_client import OpenAIClient
-from src.config import DSN
-
-
-sentry_sdk.init(
-    dsn=DSN,
-    traces_sample_rate=1.0,
-    profiles_sample_rate=1.0,
-)
 
 app = FastAPI()
 bgs = BackgroundScheduler()
