d4df9f6fbf0f5c0f61c75c2bf4ee65177714fde0
Yuannzh1 <yuanzhixu847@gmail.com>
2024-12-25 17:51:31 +0800

    extract magic number: userlength and passwordlength

--- backend/src/auth/models.py
+++ backend/src/auth/models.py
@@ -2,11 +2,12 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from src.database import Base
 from src.news.models import user_news_association_table
+from src.auth.config import USERNAME_LENGTH, PASSWORD_LENGTH
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String(50), unique=True, nullable=False)
-    hashed_password = Column(String(200), nullable=False)
+    username = Column(String(USERNAME_LENGTH), unique=True, nullable=False)
+    hashed_password = Column(String(PASSWORD_LENGTH), nullable=False)
     upvoted_news = relationship(
         "NewsArticle",
         secondary=user_news_association_table,
